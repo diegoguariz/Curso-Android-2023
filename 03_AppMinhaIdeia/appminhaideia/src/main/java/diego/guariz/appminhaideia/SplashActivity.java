c53023aa0396742f240c0131bd9c5329a9a8be0a
Diego Aparecido Guariz <48330714+diegoguariz@users.noreply.github.com>
2023-06-01 11:08:54 -0300

    Criação da Tela Splash

--- 03_AppMinhaIdeia/appminhaideia/src/main/java/diego/guariz/appminhaideia/SplashActivity.java
+++ 03_AppMinhaIdeia/appminhaideia/src/main/java/diego/guariz/appminhaideia/SplashActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
     String TAG = "APP_MINHA_IDEIA";
+
+    int tempoDeEspera = 1000 * 3; // Milisegundos, neste caso, o 3 é para representar 3 segundos
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +28,16 @@ public class SplashActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: Mudando de tela....");
 
-        Intent trocarDeTela = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(trocarDeTela);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent trocarDeTela = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(trocarDeTela);
+                finish();
+            }
+        }, tempoDeEspera);
+
+
     }
 
 }
