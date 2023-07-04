30f745b60669ecc65b95f6bb43eb6d83ef6b4501
Diego Aparecido Guariz <48330714+diegoguariz@users.noreply.github.com>
2023-07-04 16:54:49 -0300

    App Gerar QRCode em Kotlin - Criação da Classe de Geração do QRCode

--- AppGeradorQRCodeKotlin/app/src/main/java/diego/guariz/appgeradorqrcodekotlin/MainActivity.kt
+++ AppGeradorQRCodeKotlin/app/src/main/java/diego/guariz/appgeradorqrcodekotlin/MainActivity.kt
@@ -1,10 +1,18 @@
 package diego.guariz.appgeradorqrcodekotlin
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.encoder.QRCode
+import java.sql.RowId
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +23,58 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iniciarComponentes()
+
+        btnGerarQRCode!!.setOnClickListener {
+
+            if (TextUtils.isEmpty(editQRCode!!.text.toString())) {
+
+                editQRCode!!.error = "*"
+                editQRCode!!.requestFocus()
+
+            } else {
+
+                gerarQRCode(editQRCode!!.text.toString())
+
+
+            }
+
+        }
+
+    }
+
+    fun gerarQRCode(conteudoQRCode: String) {
+
+        val qrCode = QRCodeWriter()
+
+        try {
+
+            val bitMatrix = qrCode.encode(conteudoQRCode, BarcodeFormat.QR_CODE, 196, 196)
+            val tamanho = bitMatrix.width
+            val altura = bitMatrix.height
+            val bitmap = Bitmap.createBitmap(tamanho, altura, Bitmap.Config.RGB_565)
+
+            for (x in 0 until tamanho) {
+                for (y in 0 until altura) {
+
+                    bitmap.setPixel(x, y, if(bitMatrix[x, y]) Color.BLACK else Color.WHITE)
+                }
+            }
+
+            imgQRCode!!.setImageBitmap(bitmap)
+
+        } catch (e: WriterException) {
+
+
+        }
+    }
+
+    fun iniciarComponentes() {
+
+        editQRCode = findViewById(R.id.editQRCode)
+        btnGerarQRCode = findViewById(R.id.btnGerarQRCode)
+        imgQRCode = findViewById(R.id.imgQRCode)
+
     }
 }
